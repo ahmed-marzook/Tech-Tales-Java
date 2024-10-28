f954200cb01ed299885a44b434580467cc5de5c0
Ahmed Marzook <m4marzook@gmail.com>
2024-10-28 18:39:47 +0000

    Removed Ignored files

--- src/main/java/com/kaizenflow/techtales/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kaizenflow.techtales.repository;
-
-import com.kaizenflow.techtales.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
--- src/main/java/com/kaizenflow/techtales/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kaizenflow.techtales.repository;
-
-import com.kaizenflow.techtales.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
